219bd2e781a36b301d8cecd4b59788119b353900
NLindros <nils.lindros@gmail.com>
2023-08-01 11:56:23 +0200

    Add possibility to save scenarios

--- display/display.js
+++ display/display.js
@@ -1,6 +1,10 @@
+const gridSize = 15;
 let isRunning = false
 const initCells = [[0, -1], [0, 0], [0, 1]];
 let grid = { cells: initCells };
+const gridTable = document.querySelector('.grid-base');
+
+const scenarioElements = document.querySelector('.scenarios');
 
 const cellId = (x, y) => `${x},${y}`;
 
@@ -8,37 +12,60 @@ let lifeTime = 500;
 const lifeTimeSlider = document.querySelector("#life-time");
 const lifeTimeValue = document.querySelector("#life-time-value");
 lifeTimeSlider.addEventListener("input", event => {
-    lifeTime = Number(event.target.value);
-    lifeTimeValue.innerText = event.target.value;
+    lifeTime = Number(event.currentTarget.value);
+    lifeTimeValue.innerText = event.currentTarget.value;
 });
 lifeTimeSlider.value = lifeTime;
+
+function isCellAmoungCells(cell, cells) {
+    const [cell_x, cell_y] = cell;
+    return cells.findIndex(([x, y]) => { return (x == cell_x) && (y == cell_y); });
+}
 
 const toogleCell = (event) => {
     if (isRunning == true)
         return
-    const cell = event.target.id.split(',').map(Number);
-    const [cell_x, cell_y] = cell
-    const cellIndex = grid.cells.findIndex(([x, y]) => { return (x == cell_x) && (y == cell_y) });
+    const cell = event.currentTarget.id.split(',').map(Number);
+    const cellIndex = isCellAmoungCells(cell, grid.cells);
     if (cellIndex >= 0) {
         grid.cells = grid.cells.filter((item, index) => index != cellIndex);
-        event.target.classList.remove('active');
+        event.currentTarget.classList.remove('active');
     } else {
-        grid.cells = [...grid.cells, cell];
-        event.target.classList.add('active');
+        grid.cells = [...grid.cells, cell].sort(sort2DByRows).sort(sort2DByCols);
+        event.currentTarget.classList.add('active');
     }
+    console.log(grid.cells);
 }
 
+const clampToOne = val => { return val === 0 ? 0 : val > 0 ? 1 : -1 }
 
-const resetButtonClick = () => {
-    grid.cells = initCells;
-    clearBoard();
-    drawBoard(grid);
+const sort2DByCols = (a, b) => { return clampToOne(a[0] - b[0]) };
+const sort2DByRows = (a, b) => { return clampToOne(a[1] - b[1]) };
+
+const saveButtonClick = () => {
+    saveScenario(grid.cells)
+}
+
+const createMiniGrid = (cells) => {
+    const scenarioTable = document.createElement('table');
+    let row;
+    for (let y = -gridSize; y < gridSize; y++) {
+        row = document.createElement('tr');
+        for (let x = -gridSize; x < gridSize; x++) {
+            cellElement = document.createElement('td');
+            cellElement.classList.add('cell');
+            if (isCellAmoungCells([x, y], cells) >= 0) {
+                cellElement.classList.add('active')
+            }
+            row.appendChild(cellElement)
+        }
+        scenarioTable.appendChild(row);
+    }
+    return scenarioTable;
 }
 
 const createGrid = () => {
-    const gridTable = document.querySelector('.grid-base');
     let row, cell;
-    const gridSize = 15;
     for (let y = -gridSize; y < gridSize; y++) {
         row = document.createElement('tr');
         for (let x = -gridSize; x < gridSize; x++) {
@@ -53,16 +80,42 @@ const createGrid = () => {
 }
 
 const clearBoard = () => {
-    const cells = document.getElementsByClassName('cell');
+    const cells = gridTable.getElementsByClassName('cell');
     for (let cell of cells) {
         cell.classList.remove("active");
     }
 }
 
-const drawBoard = grid => {
-    grid.cells.forEach(cell =>
+const drawBoard = cells => {
+    cells.forEach(cell =>
         document.getElementById(cellId(...cell)).classList.add("active")
     )
+}
+
+const collectPairs = flatCells => {
+    return flatCells
+        .split(",")
+        .map(Number)
+        .reduce((result, val, index, array) =>
+            index % 2 === 0 ? [...result, array.slice(index, index + 2)] : result
+            , [])
+}
+
+const loadScenario = (event) => {
+    const scenario = event.currentTarget;
+    grid.cells = collectPairs(scenario.dataset.cells);
+    clearBoard();
+    drawBoard(grid.cells);
+}
+
+const stepLife = async () => {
+    grid = await getNextGrid(grid);
+    clearBoard();
+    drawBoard(grid.cells);
+}
+
+const stepButtonClick = async () => {
+    await stepLife();
 }
 
 const runButtonClick = () => {
@@ -70,15 +123,13 @@ const runButtonClick = () => {
     isRunning = !isRunning;
     const btnText = isRunning ? "Pause" : "Run";
     btn.innerText = btnText;
-    lifeStep()
+    runLife()
 }
 
-const lifeStep = async () => {
+const runLife = async () => {
     if (isRunning == true) {
-        grid = await getNextGrid(grid)
-        clearBoard();
-        drawBoard(grid);
-        setTimeout(lifeStep, lifeTime)
+        await stepLife();
+        setTimeout(runLife, lifeTime);
     }
 }
 
@@ -95,4 +146,16 @@ const getNextGrid = async (data) => {
 }
 
 createGrid();
-drawBoard(grid);
+drawBoard(grid.cells);
+
+
+const saveScenario = (cells) => {
+    const scenario = document.createElement('div')
+    scenario.addEventListener('click', loadScenario)
+    scenario.classList.add('scenario')
+    scenario.setAttribute('data-cells', cells)
+    scenario.appendChild(createMiniGrid(cells))
+    scenarioElements.appendChild(scenario)
+}
+
+
